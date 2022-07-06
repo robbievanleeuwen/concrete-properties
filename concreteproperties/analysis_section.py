Add rotation tests, various bug fixes

concreteproperties/analysis_section.py
@@ -348,7 +348,12 @@ class AnalysisSection:
         if n == 0:
             d = 0
         else:
-            d = mv / n - pc_local - point_na[1]
+            # get principal coordinates of neutral axis
+            na_local = utils.principal_coordinate(
+                phi=theta * 180 / np.pi, x=point_na[0], y=point_na[1]
+            )
+
+            d = mv / n + pc_local - na_local[1]
 
         return sig, n, d
 
@@ -527,13 +532,10 @@ class Tri3:
         """
 
         # initialise element results
-        area_e = 0
-        qx_e = 0
-        qy_e = 0
         force_e = 0
         mv_e = 0
 
-        # get points for 1 point Gaussian integration
+        # get points for 3 point Gaussian integration
         gps = utils.gauss_points(n=3)
 
         # loop through each gauss point
@@ -544,11 +546,6 @@ class Tri3:
             # get coordinates (wrt NA) of the gauss point
             x = np.dot(N, np.transpose(self.coords[0, :])) - cx
             y = np.dot(N, np.transpose(self.coords[1, :])) - cy
-
-            # calculate area properties
-            area_e += gp[0] * j
-            qx_e += gp[0] * y * j
-            qy_e += gp[0] * x * j
 
             # axial force
             force_gp = 0
@@ -605,10 +602,8 @@ class Tri3:
         """
 
         # initialise element results
-        area_e = 0
-        qx_e = 0
-        qy_e = 0
         force_e = 0
+        mv_e = 0
 
         # get points for 1 point Gaussian integration
         gps = utils.gauss_points(n=1)
@@ -622,11 +617,6 @@ class Tri3:
             x = np.dot(N, np.transpose(self.coords[0, :]))
             y = np.dot(N, np.transpose(self.coords[1, :]))
 
-            # calculate area properties
-            area_e += gp[0] * j
-            qx_e += gp[0] * y * j
-            qy_e += gp[0] * x * j
-
             # get strain at gauss point
             strain = utils.get_service_strain(
                 point=(x, y),
@@ -639,18 +629,16 @@ class Tri3:
             stress = self.conc_material.stress_strain_profile.get_stress(strain=strain)
 
             # calculate force (stress * area)
-            force_e += gp[0] * stress * j
+            force_gp = gp[0] * stress * j
 
-        # calculate element centroid
-        cx_e, cy_e = qy_e / area_e, qx_e / area_e
+            # convert gauss point to local coordinates
+            _, c_v = sp_fea.principal_coordinate(phi=theta * 180 / np.pi, x=x, y=y)
 
-        # convert centroid to local coordinates
-        _, c_v = sp_fea.principal_coordinate(phi=theta * 180 / np.pi, x=cx_e, y=cy_e)
+            # add force and moment
+            force_e += force_gp
+            mv_e += force_gp * (c_v - na_local)
 
-        # calculate moment
-        mv = force_e * (c_v - na_local)
-
-        return force_e, mv
+        return force_e, mv_e
 
     def calculate_ultimate_actions(
         self,
@@ -674,10 +662,8 @@ class Tri3:
         """
 
         # initialise element results
-        area_e = 0
-        qx_e = 0
-        qy_e = 0
         force_e = 0
+        mv_e = 0
 
         # get points for 1 point Gaussian integration
         gps = utils.gauss_points(n=1)
@@ -690,11 +676,6 @@ class Tri3:
             # get coordinates of the gauss point
             x = np.dot(N, np.transpose(self.coords[0, :]))
             y = np.dot(N, np.transpose(self.coords[1, :]))
-
-            # calculate area properties
-            area_e += gp[0] * j
-            qx_e += gp[0] * y * j
-            qy_e += gp[0] * x * j
 
             # get strain at gauss point
             strain = utils.get_ultimate_strain(
@@ -711,15 +692,13 @@ class Tri3:
             )
 
             # calculate force (stress * area)
-            force_e += gp[0] * stress * j
+            force_gp = gp[0] * stress * j
 
-        # calculate element centroid
-        cx_e, cy_e = qy_e / area_e, qx_e / area_e
+            # convert gauss point to local coordinates
+            _, c_v = sp_fea.principal_coordinate(phi=theta * 180 / np.pi, x=x, y=y)
 
-        # convert centroid to local coordinates
-        _, c_v = sp_fea.principal_coordinate(phi=theta * 180 / np.pi, x=cx_e, y=cy_e)
+            # add force and moment
+            force_e += force_gp
+            mv_e += force_gp * (c_v - pc_local)
 
-        # calculate moment
-        mv = force_e * (c_v - pc_local)
-
-        return force_e, mv
+        return force_e, mv_e

concreteproperties/concrete_section.py
@@ -1073,120 +1073,6 @@ class ConcreteSection:
 
         return bb_results
 
-    def get_c_local(
-        self,
-        theta: float,
-    ) -> Tuple[float]:
-        """Returns the elastic centroid location in local coordinates.
-
-        :param float theta: Angle the neutral axis makes with the horizontal axis
-
-        :return: Elastic centroid in local coordinates `(c_u, c_v)`
-        :rtype: Tuple[float]
-        """
-
-        return principal_coordinate(
-            phi=theta * 180 / np.pi,
-            x=self.gross_properties.cx,
-            y=self.gross_properties.cy,
-        )
-
-    def get_pc_local(
-        self,
-        theta: float,
-    ) -> Tuple[float]:
-        """Returns the plastic centroid location in local coordinates.
-
-        :param float theta: Angle the neutral axis makes with the horizontal axis
-
-        :return: Plastic centroid in local coordinates `(pc_u, pc_v)`
-        :rtype: Tuple[float]
-        """
-
-        return principal_coordinate(
-            phi=theta * 180 / np.pi,
-            x=self.gross_properties.axial_pc_x,
-            y=self.gross_properties.axial_pc_y,
-        )
-
-    def plot_section(
-        self,
-        title: Optional[str] = "Reinforced Concrete Section",
-        background: Optional[bool] = False,
-        **kwargs,
-    ) -> matplotlib.axes._subplots.AxesSubplot:
-        """Plots the reinforced concrete section.
-
-        :param title: Plot title
-        :type title: Optional[str]
-        :param background: If set to True, uses the plot as a background plot
-        :type background: Optional[bool]
-        :param kwargs: Passed to :func:`~concreteproperties.post.plotting_context`
-
-        :return: Matplotlib axes object
-        :rtype: :class:`matplotlib.axes._subplots.AxesSubplot`
-        """
-
-        with plotting_context(title=title, **kwargs) as (fig, ax):
-            # create list of already plotted materials
-            plotted_materials = []
-            legend_labels = []
-
-            # plot concrete geometries
-            for conc_geom in self.concrete_geometries:
-                if conc_geom.material not in plotted_materials:
-                    patch = mpatches.Patch(
-                        color=conc_geom.material.colour, label=conc_geom.material.name
-                    )
-                    legend_labels.append(patch)
-                    plotted_materials.append(conc_geom.material)
-
-                # TODO - when shapely implements polygon plotting, fix this up
-                sec = AnalysisSection(geometry=conc_geom)
-
-                if not background:
-                    sec.plot_shape(ax=ax)
-
-                # plot the points and facets
-                for f in conc_geom.facets:
-                    if background:
-                        fmt = "k-"
-                    else:
-                        fmt = "ko-"
-
-                    ax.plot(
-                        [conc_geom.points[f[0]][0], conc_geom.points[f[1]][0]],
-                        [conc_geom.points[f[0]][1], conc_geom.points[f[1]][1]],
-                        fmt,
-                        markersize=2,
-                        linewidth=1.5,
-                    )
-
-            # plot steel geometries
-            for steel_geom in self.steel_geometries:
-                if steel_geom.material not in plotted_materials:
-                    patch = mpatches.Patch(
-                        color=steel_geom.material.colour, label=steel_geom.material.name
-                    )
-                    legend_labels.append(patch)
-                    plotted_materials.append(steel_geom.material)
-
-                # plot the points and facets
-                coords = list(steel_geom.geom.exterior.coords)
-                bar = mpatches.Polygon(
-                    xy=coords, closed=False, color=steel_geom.material.colour
-                )
-                ax.add_patch(bar)
-
-            if not background:
-                ax.legend(
-                    loc="center left", bbox_to_anchor=(1, 0.5), handles=legend_labels
-                )
-
-            ax.set_aspect("equal", anchor="C")
-
-        return ax
-
     def calculate_uncracked_stress(
         self,
         n: Optional[float] = 0,
@@ -1226,7 +1112,7 @@ class ConcreteSection:
         e_ixy = self.gross_properties.e_ixy_c
 
         # calculate neutral axis rotation
-        theta = np.arctan2(my, mx)
+        theta = np.arctan2(-my, mx)
 
         # point on neutral axis is centroid
         point_na = (cx, cy)
@@ -1666,3 +1552,117 @@ class ConcreteSection:
             steel_stresses=steel_sigs,
             steel_forces=steel_forces,
         )
+
+    def get_c_local(
+        self,
+        theta: float,
+    ) -> Tuple[float]:
+        """Returns the elastic centroid location in local coordinates.
+
+        :param float theta: Angle the neutral axis makes with the horizontal axis
+
+        :return: Elastic centroid in local coordinates `(c_u, c_v)`
+        :rtype: Tuple[float]
+        """
+
+        return principal_coordinate(
+            phi=theta * 180 / np.pi,
+            x=self.gross_properties.cx,
+            y=self.gross_properties.cy,
+        )
+
+    def get_pc_local(
+        self,
+        theta: float,
+    ) -> Tuple[float]:
+        """Returns the plastic centroid location in local coordinates.
+
+        :param float theta: Angle the neutral axis makes with the horizontal axis
+
+        :return: Plastic centroid in local coordinates `(pc_u, pc_v)`
+        :rtype: Tuple[float]
+        """
+
+        return principal_coordinate(
+            phi=theta * 180 / np.pi,
+            x=self.gross_properties.axial_pc_x,
+            y=self.gross_properties.axial_pc_y,
+        )
+
+    def plot_section(
+        self,
+        title: Optional[str] = "Reinforced Concrete Section",
+        background: Optional[bool] = False,
+        **kwargs,
+    ) -> matplotlib.axes._subplots.AxesSubplot:
+        """Plots the reinforced concrete section.
+
+        :param title: Plot title
+        :type title: Optional[str]
+        :param background: If set to True, uses the plot as a background plot
+        :type background: Optional[bool]
+        :param kwargs: Passed to :func:`~concreteproperties.post.plotting_context`
+
+        :return: Matplotlib axes object
+        :rtype: :class:`matplotlib.axes._subplots.AxesSubplot`
+        """
+
+        with plotting_context(title=title, **kwargs) as (fig, ax):
+            # create list of already plotted materials
+            plotted_materials = []
+            legend_labels = []
+
+            # plot concrete geometries
+            for conc_geom in self.concrete_geometries:
+                if conc_geom.material not in plotted_materials:
+                    patch = mpatches.Patch(
+                        color=conc_geom.material.colour, label=conc_geom.material.name
+                    )
+                    legend_labels.append(patch)
+                    plotted_materials.append(conc_geom.material)
+
+                # TODO - when shapely implements polygon plotting, fix this up
+                sec = AnalysisSection(geometry=conc_geom)
+
+                if not background:
+                    sec.plot_shape(ax=ax)
+
+                # plot the points and facets
+                for f in conc_geom.facets:
+                    if background:
+                        fmt = "k-"
+                    else:
+                        fmt = "ko-"
+
+                    ax.plot(
+                        [conc_geom.points[f[0]][0], conc_geom.points[f[1]][0]],
+                        [conc_geom.points[f[0]][1], conc_geom.points[f[1]][1]],
+                        fmt,
+                        markersize=2,
+                        linewidth=1.5,
+                    )
+
+            # plot steel geometries
+            for steel_geom in self.steel_geometries:
+                if steel_geom.material not in plotted_materials:
+                    patch = mpatches.Patch(
+                        color=steel_geom.material.colour, label=steel_geom.material.name
+                    )
+                    legend_labels.append(patch)
+                    plotted_materials.append(steel_geom.material)
+
+                # plot the points and facets
+                coords = list(steel_geom.geom.exterior.coords)
+                bar = mpatches.Polygon(
+                    xy=coords, closed=False, color=steel_geom.material.colour
+                )
+                ax.add_patch(bar)
+
+            if not background:
+                ax.legend(
+                    loc="center left", bbox_to_anchor=(1, 0.5), handles=legend_labels
+                )
+
+            ax.set_aspect("equal", anchor="C")
+
+        return ax