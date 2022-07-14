593669f21db7f3c32f1908e8ee20851b3181dc29
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2022-07-14 22:48:53 +1000

    Fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,8 @@ html_theme_options = {
     "navbar_start": ["navbar-logo", "version-switcher"],
     "switcher": {
         "json_url": json_url,
-        "version_match": "latest",
+        # "version_match": "latest",
+        "version_match": release,
     },
 }
 
