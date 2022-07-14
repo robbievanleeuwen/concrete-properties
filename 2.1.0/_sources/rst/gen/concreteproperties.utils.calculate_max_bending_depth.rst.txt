042e1c179a1fb15ebc299832728f55f191f18322
Robbie van Leeuwen <robbie.vanleeuwen@gmail.com>
2022-07-14 22:20:06 +1000

    Delete 2.1.0 directory

--- 2.1.0/_sources/rst/gen/concreteproperties.utils.calculate_max_bending_depth.rst.txt
+++
@@ -1,6 +0,0 @@
-concreteproperties.utils.calculate\_max\_bending\_depth
-=======================================================
-
-.. currentmodule:: concreteproperties.utils
-
-.. autofunction:: calculate_max_bending_depth
--- 2.1.0/rst/materials-9.py
+++
@@ -1,9 +0,0 @@
-from concreteproperties.stress_strain_profile import SteelProfile
-
-SteelProfile(
-  strains=[-0.05, -0.03, -0.02, -500 / 200e3, 0, 500 / 200e3, 0.02, 0.03, 0.05],
-  stresses=[-600, -600, -500, -500, 0, 500, 500, 600, 600],
-  yield_strength=500,
-  elastic_modulus=200e3,
-  fracture_strain=0.05,
-).plot_stress_strain()
