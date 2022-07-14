0e883979ee9c450615df80910c23f93010336951
GitHub Action <action@github.com>
2022-07-14 10:03:59 +0000

    Update documentation

--- _static/anim/gen.py
+++ _static/anim/gen.py
@@ -8,5 +8,11 @@ fp_out = "anim.gif"
 # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 imgs = (Image.open(f) for f in sorted(glob.glob(fp_in)))
 img = next(imgs)  # extract first image from iterator
-img.save(fp=fp_out, format='GIF', append_images=imgs,
-         save_all=True, duration=1000/24, loop=0)
+img.save(
+    fp=fp_out,
+    format="GIF",
+    append_images=imgs,
+    save_all=True,
+    duration=1000 / 24,
+    loop=0,
+)
