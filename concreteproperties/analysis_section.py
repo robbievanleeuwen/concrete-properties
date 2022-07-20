Uncracked and cracked lever arms now in x&y direction; fix uncracked stress NA rotation

concreteproperties/analysis_section.py
@@ -85,8 +85,7 @@ class AnalysisSection:
         e_ixx: float,
         e_iyy: float,
         e_ixy: float,
-        theta: float,
-    ) -> Tuple[np.ndarray, float, float]:
+    ) -> Tuple[np.ndarray, float, float, float]:
         r"""Given section actions and section propreties, calculates elastic stresses.
 
         :param float n: Axial force
@@ -98,12 +97,10 @@ class AnalysisSection:
         :param float e_ixx: Flexural rigidity about the x-axis
         :param float e_iyy: Flexural rigidity about the y-axis
         :param float e_ixy: Flexural rigidity about the xy-axis
-        :param float theta: Angle (in radians) the neutral axis makes with the
-            horizontal axis (:math:`-\pi \leq \theta \leq \pi`)
 
         :return: Elastic stresses, net force and distance from neutral axis to point of
             force action
-        :rtype: Tuple[:class:`numpy.ndarray`, float, float]
+        :rtype: Tuple[:class:`numpy.ndarray`, float, float, float]
         """
 
         # intialise stress results
@@ -129,10 +126,11 @@ class AnalysisSection:
 
         # initialise section actions
         n_conc = 0
-        m_u = 0
+        m_x_conc = 0
+        m_y_conc = 0
 
         for el in self.elements:
-            el_n, el_m_u = el.calculate_elastic_actions(
+            el_n, el_m_x, el_m_y = el.calculate_elastic_actions(
                 n=n,
                 m_x=m_x,
                 m_y=m_y,
@@ -142,19 +140,21 @@ class AnalysisSection:
                 e_ixx=e_ixx,
                 e_iyy=e_iyy,
                 e_ixy=e_ixy,
-                theta=theta,
             )
 
             n_conc += el_n
-            m_u += el_m_u
+            m_x_conc += el_m_x
+            m_y_conc += el_m_y
 
         # calculate point of action
         if n_conc == 0:
-            d = 0
+            d_x = 0
+            d_y = 0
         else:
-            d = m_u / n_conc
+            d_x = m_y_conc / n_conc
+            d_y = m_x_conc / n_conc
 
-        return sig, n_conc, d
+        return sig, n_conc, d_x, d_y
 
     def service_stress_analysis(
         self,
@@ -517,7 +517,6 @@ class Tri3:
         e_ixx: float,
         e_iyy: float,
         e_ixy: float,
-        theta: float,
     ) -> Tuple[float]:
         r"""Calculates elastic actions for the current finite element.
 
@@ -530,8 +529,6 @@ class Tri3:
         :param float e_ixx: Flexural rigidity about the x-axis
         :param float e_iyy: Flexural rigidity about the y-axis
         :param float e_ixy: Flexural rigidity about the xy-axis
-        :param float theta: Angle (in radians) the neutral axis makes with the
-            horizontal axis (:math:`-\pi \leq \theta \leq \pi`)
 
         :return: Elastic force and resultant moment
         :rtype: Tuple[float]
@@ -539,7 +536,8 @@ class Tri3:
 
         # initialise element results
         force_e = 0
-        m_u_e = 0
+        m_x_e = 0
+        m_y_e = 0
 
         # get points for 3 point Gaussian integration
         gps = utils.gauss_points(n=3)
@@ -577,14 +575,12 @@ class Tri3:
                 * j
             )
 
-            # convert gauss point to local coordinates
-            _, c_v = sp_fea.principal_coordinate(phi=theta * 180 / np.pi, x=x, y=y)
-
             # add force and moment
             force_e += force_gp
-            m_u_e += force_gp * c_v
+            m_x_e += force_gp * y
+            m_y_e += force_gp * x
 
-        return force_e, m_u_e
+        return force_e, m_x_e, m_y_e
 
     def calculate_service_actions(
         self,

concreteproperties/concrete_section.py
@@ -1275,15 +1275,14 @@ class ConcreteSection:
         e_ixy = self.gross_properties.e_ixy_c
 
         # calculate neutral axis rotation
-        theta = np.arctan2(-m_y, m_x)
+        grad = (e_ixy * m_x - e_ixx * m_y) / (e_iyy * m_x - e_ixy * m_y)
+        theta = np.arctan2(grad, 1)
+
+        if np.isclose(theta, 0):
+            theta = 0
 
         # point on neutral axis is centroid
         point_na = (cx, cy)
-
-        # get principal coordinates of neutral axis
-        na_local = principal_coordinate(
-            phi=theta * 180 / np.pi, x=point_na[0], y=point_na[1]
-        )
 
         # split concrete geometries above and below neutral axis
         split_conc_geoms = []
@@ -1303,7 +1302,7 @@ class ConcreteSection:
             analysis_section = AnalysisSection(geometry=conc_geom)
 
             # calculate stress, force and point of action
-            sig, n_conc, d = analysis_section.get_elastic_stress(
+            sig, n_conc, d_x, d_y = analysis_section.get_elastic_stress(
                 n=n,
                 m_x=m_x,
                 m_y=m_y,
@@ -1313,10 +1312,9 @@ class ConcreteSection:
                 e_ixx=e_ixx,
                 e_iyy=e_iyy,
                 e_ixy=e_ixy,
-                theta=theta,
             )
             conc_sigs.append(sig)
-            conc_forces.append((n_conc, d))
+            conc_forces.append((n_conc, d_x, d_y))
 
             # save analysis section
             analysis_sections.append(analysis_section)
@@ -1345,14 +1343,10 @@ class ConcreteSection:
 
             # net force and point of action
             n_steel = sig * steel_geom.calculate_area()
-            _, c_v = principal_coordinate(
-                phi=theta * 180 / np.pi, x=centroid[0], y=centroid[1]
-            )
-            d = c_v - na_local[1]
 
             steel_sigs.append(sig)
             steel_strains.append(strain)
-            steel_forces.append((n_steel, d))
+            steel_forces.append((n_steel, x, y))
 
         return res.StressResult(
             concrete_section=self,
@@ -1432,8 +1426,6 @@ class ConcreteSection:
         m_x = sign * np.sqrt(m * m / (1 + 1 / (c * c)))
         m_y = m_x / c
 
-        print(m_x/1e6, m_y/1e6)
-
         # depth of neutral axis at extreme tensile fibre
         extreme_fibre, d_t = utils.calculate_extreme_fibre(
             points=self.geometry.points, theta=theta
@@ -1455,7 +1447,7 @@ class ConcreteSection:
                 analysis_section = AnalysisSection(geometry=geom)
 
                 # calculate stress, force and point of action
-                sig, n_conc, d = analysis_section.get_elastic_stress(
+                sig, n_conc, d_x, d_y = analysis_section.get_elastic_stress(
                     n=n,
                     m_x=m_x,
                     m_y=m_y,
@@ -1465,10 +1457,9 @@ class ConcreteSection:
                     e_ixx=e_ixx,
                     e_iyy=e_iyy,
                     e_ixy=e_ixy,
-                    theta=theta,
                 )
                 conc_sigs.append(sig)
-                conc_forces.append((n_conc, d))
+                conc_forces.append((n_conc, d_x, d_y))
 
                 # save analysis section
                 analysis_sections.append(analysis_section)
@@ -1497,14 +1488,14 @@ class ConcreteSection:
 
             # net force and point of action
             n_steel = sig * steel_geom.calculate_area()
-            _, c_v = principal_coordinate(
-                phi=theta * 180 / np.pi, x=centroid[0], y=centroid[1]
-            )
-            d = c_v - na_local[1]
+            # _, c_v = principal_coordinate(
+            #     phi=theta * 180 / np.pi, x=centroid[0], y=centroid[1]
+            # )
+            # d = c_v - na_local[1]
 
             steel_sigs.append(sig)
             steel_strains.append(strain)
-            steel_forces.append((n_steel, d))
+            steel_forces.append((n_steel, x, y))
 
         return res.StressResult(
             concrete_section=self,
@@ -1615,7 +1606,7 @@ class ConcreteSection:
                 na_local=na_local[1],
             )
             conc_sigs.append(sig)
-            conc_forces.append((n_conc, d))
+            conc_forces.append((n_conc, d, 0))
 
             # save analysis section
             analysis_sections.append(analysis_section)
@@ -1643,7 +1634,7 @@ class ConcreteSection:
 
             steel_sigs.append(sig)
             steel_strains.append(strain)
-            steel_forces.append((n_steel, d))
+            steel_forces.append((n_steel, d, 0))
 
         return res.StressResult(
             concrete_section=self,
@@ -1721,7 +1712,7 @@ class ConcreteSection:
                 pc_local=pc_local[1],
             )
             conc_sigs.append(sig)
-            conc_forces.append((n_conc, d))
+            conc_forces.append((n_conc, d, 0))
 
             # save analysis section
             analysis_sections.append(analysis_section)
@@ -1750,7 +1741,7 @@ class ConcreteSection:
 
             steel_sigs.append(sig)
             steel_strains.append(strain)
-            steel_forces.append((n_steel, d))
+            steel_forces.append((n_steel, d, 0))
 
         return res.StressResult(
             concrete_section=self,

concreteproperties/results.py
@@ -953,6 +953,9 @@ class BiaxialBendingResults:
 class StressResult:
     """Class for storing stress results.
 
+    For service and ultimate stress analyses, the lever arm is stored in the ``d_x``
+    variable and is the perpendicular distance to the neutral axis.
+
     :var concrete_analysis_sections: List of concrete analysis section objects
         present in the stress analysis, which can be visualised by calling the
         :meth:`~concreteproperties.analysis_section.AnalysisSection.plot_mesh` or
@@ -963,7 +966,7 @@ class StressResult:
         analysis section
     :vartype concrete_stresses: List[:class:`numpy.ndarray`]
     :var concrete_forces: List of net forces for each concrete analysis section and its
-        lever arm to the neutral axis (``force``, ``lever_arm``)
+        lever arm to the neutral axis (``force``, ``d_x``, ``d_y``)
     :vartype concrete_forces: List[Tuple[float]]
     :var steel_geometries: List of steel geometry objects present in the stress analysis
     :vartype steel_geometries: List[:class:`sectionproperties.pre.geometry.Geometry`]
@@ -972,7 +975,7 @@ class StressResult:
     :var steel_strains: List of steel strains for each steel geometry
     :vartype steel_strains: List[float]
     :var steel_forces: List of net forces for each steel geometry and its lever arm to
-        the neutral axis (``force``, ``lever_arm``)
+        the neutral axis (``force``, ``d_x``, ``d_y``)
     :vartype steel_forces: List[Tuple[float]]
     """
 
@@ -1154,21 +1157,27 @@ class StressResult:
 
     def sum_moments(
         self,
-    ) -> float:
+    ) -> Tuple[float]:
         """Returns the sum of the internal moments.
 
-        :return: Sum of internal moments
-        :rtype: float
+        :return: Sum of internal moments about each axis and resultant moment
+            (``m_x``, ``m_y``, ``m``)
+        :rtype: Tuple[float]
         """
 
-        moment_sum = 0
+        moment_sum_x = 0
+        moment_sum_y = 0
 
         # sum concrete forces
         for conc_force in self.concrete_forces:
-            moment_sum += conc_force[0] * conc_force[1]
+            moment_sum_x += conc_force[0] * conc_force[2]
+            moment_sum_y += conc_force[0] * conc_force[1]
 
         # sum steel forces
         for steel_force in self.steel_forces:
-            moment_sum += steel_force[0] * steel_force[1]
+            moment_sum_x += steel_force[0] * steel_force[2]
+            moment_sum_y += steel_force[0] * steel_force[1]
 
-        return moment_sum
+        moment_sum = np.sqrt(moment_sum_x * moment_sum_x + moment_sum_y * moment_sum_y)
+
+        return moment_sum_x, moment_sum_y, moment_sum

concreteproperties/tests/test_rotation.py
@@ -147,13 +147,17 @@ def test_rotated_uncracked_stress(theta):
         ref_uncr_stress = ref_sec.calculate_uncracked_stress(n=nf, m_x=m)
         new_uncr_stress = new_sec.calculate_uncracked_stress(n=nf, m_x=m_x, m_y=m_y)
 
+        # fix top and bottom geometries
         for idx, cf in enumerate(new_uncr_stress.concrete_forces):
-            assert pytest.approx(cf[0]) == ref_uncr_stress.concrete_forces[idx][0]
-            assert pytest.approx(cf[1]) == ref_uncr_stress.concrete_forces[idx][1]
+            if theta < -np.pi / 2 or theta > np.pi /2:
+                i = len(new_uncr_stress.concrete_forces) - idx - 1
+            else:
+                i = idx
+
+            assert pytest.approx(cf[0]) == ref_uncr_stress.concrete_forces[i][0]
 
         for idx, sf in enumerate(new_uncr_stress.steel_forces):
             assert pytest.approx(sf[0]) == ref_uncr_stress.steel_forces[idx][0]
-            assert pytest.approx(sf[1]) == ref_uncr_stress.steel_forces[idx][1]
 
 
 # list of normal forces
@@ -180,11 +184,9 @@ def test_rotated_cracked_stress(theta):
 
         for idx, cf in enumerate(new_cr_stress.concrete_forces):
             assert pytest.approx(cf[0]) == ref_cr_stress.concrete_forces[idx][0]
-            assert pytest.approx(cf[1]) == ref_cr_stress.concrete_forces[idx][1]
 
         for idx, sf in enumerate(new_cr_stress.steel_forces):
             assert pytest.approx(sf[0]) == ref_cr_stress.steel_forces[idx][0]
-            assert pytest.approx(sf[1]) == ref_cr_stress.steel_forces[idx][1]
 
 
 # list of normal forces
@@ -210,8 +212,6 @@ def test_rotated_ultimate_stress(theta):
 
         for idx, cf in enumerate(new_ult_stress.concrete_forces):
             assert pytest.approx(cf[0]) == ref_ult_stress.concrete_forces[idx][0]
-            assert pytest.approx(cf[1]) == ref_ult_stress.concrete_forces[idx][1]
 
         for idx, sf in enumerate(new_ult_stress.steel_forces):
             assert pytest.approx(sf[0]) == ref_ult_stress.steel_forces[idx][0]
-            assert pytest.approx(sf[1]) == ref_ult_stress.steel_forces[idx][1]