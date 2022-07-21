968dedae688c6605874c990938ad9b0b6b369e39
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2022-07-21 14:14:08 +1000

    Change docs back to latest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,8 +118,8 @@ html_theme_options = {
     "navbar_start": ["navbar-logo", "version-switcher"],
     "switcher": {
         "json_url": json_url,
-        # "version_match": "latest",
-        "version_match": release,
+        "version_match": "latest",
+        # "version_match": release,
     },
 }
 
