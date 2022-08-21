dfcd7802f23ed7d7d71da9ac1aa1272e0ed08fda
Agent6-6-6 <36808164+Agent6-6-6@users.noreply.github.com>
2022-08-21 18:12:28 +1200

    update post.py

--- concreteproperties/post.py
+++ concreteproperties/post.py
@@ -14,6 +14,7 @@ def plotting_context(
     ax: Optional[matplotlib.axes.Axes] = None,  # type: ignore
     pause: bool = True,
     title: str = "",
+    aspect: bool = False,
     filename: str = "",
     render: bool = True,
     axis_index: Optional[Union[int, Tuple[int]]] = None,
@@ -78,6 +79,9 @@ def plotting_context(
     # if no axes was supplied, finish the plot and return the figure and axes
     plt.tight_layout()
 
+    if aspect:
+        ax.set_aspect("equal", anchor="C")  # type: ignore
+        
     if filename:
         fig.savefig(filename, dpi=fig.dpi)
         plt.close(fig)  # close the figure to free the memory
