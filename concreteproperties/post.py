52dc0aca9086670306c875a9938aac6749cfdb5f
Agent6-6-6 <36808164+Agent6-6-6@users.noreply.github.com>
2022-08-22 21:49:17 +1200

    add aspect variable to docstring

--- concreteproperties/post.py
+++ concreteproperties/post.py
@@ -27,6 +27,8 @@ def plotting_context(
         closed. If set to false, the script continues immediately after the window is
         rendered.
     :param title: Plot title
+    :param aspect: If set to True, the axes of the figure are set to an equal aspect
+        ratio
     :param filename: Pass a non-empty string or path to save the image as. If
         this option is used, the figure is closed after the file is saved.
     :param render: If set to False, the image is not displayed. This may be useful
@@ -81,7 +83,7 @@ def plotting_context(
 
     if aspect:
         ax.set_aspect("equal", anchor="C")  # type: ignore
-        
+
     if filename:
         fig.savefig(filename, dpi=fig.dpi)
         plt.close(fig)  # close the figure to free the memory
