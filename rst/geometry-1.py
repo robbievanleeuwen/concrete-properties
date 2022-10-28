7c61b446b64e2f80ad22f52510b2407bd78fefe6
Robbie van Leeuwen <robbie.vanleeuwen@gmail.com>
2022-10-28 17:04:25 +1100

    Delete rst directory

--- rst/geometry-1.py
+++
@@ -1,5 +0,0 @@
-from sectionproperties.pre.library.primitive_sections import rectangular_section
-
-concrete = None  # define your concrete material properties here
-geom = rectangular_section(d=600, b=300, material=concrete)
-geom.plot_geometry(labels=[], cp=False, legend=False)
