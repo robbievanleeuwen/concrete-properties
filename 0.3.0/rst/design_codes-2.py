04475a328d11286201717b48ac75db3c34395517
Robbie van Leeuwen <robbie.vanleeuwen@gmail.com>
2022-12-20 20:35:44 +1100

    Delete 0.3.0 directory

--- 0.3.0/rst/design_codes-2.py
+++
@@ -1,8 +0,0 @@
-from concreteproperties.design_codes import AS3600
-
-design_code = AS3600()
-steel = design_code.create_steel_material()
-
-steel.stress_strain_profile.plot_stress_strain(
-  title=f"{steel.name} - Stress-Strain Profile"
-)
--- 0.3.0/rst/materials-12.py
+++
@@ -1,8 +0,0 @@
-from concreteproperties.stress_strain_profile import SteelHardening
-
-SteelHardening(
-  yield_strength=500,
-  elastic_modulus=200e3,
-  fracture_strain=0.05,
-  ultimate_strength=600,
-).plot_stress_strain()
