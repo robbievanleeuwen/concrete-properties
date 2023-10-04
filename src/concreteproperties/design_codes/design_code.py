b32d5f8b772f7d662bda3e801cb55fc60c4ccf07
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2023-10-04 18:04:10 +1100

    Update design_code.py

--- src/concreteproperties/design_codes/design_code.py
+++ src/concreteproperties/design_codes/design_code.py
@@ -1,33 +1,33 @@
+"""Abstract class for a design code."""
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
 import concreteproperties.results as res
-from concreteproperties.material import Concrete, SteelBar
+
 
 if TYPE_CHECKING:
     from concreteproperties.concrete_section import ConcreteSection
+    from concreteproperties.material import Concrete, SteelBar
 
 
 class DesignCode:
     """Abstract class for a design code object."""
 
-    def __init__(
-        self,
-    ):
+    def __init__(self) -> None:
         """Inits the DesignCode class."""
-
         pass
 
     def assign_concrete_section(
         self,
         concrete_section: ConcreteSection,
-    ):
+    ) -> None:
         """Assigns a concrete section to the design code.
 
-        :param concrete_section: Concrete section object to analyse
+        Args:
+            concrete_section: Concrete section object to analyse
         """
-
         self.concrete_section = concrete_section
 
     def create_concrete_material(
@@ -39,12 +39,13 @@ class DesignCode:
 
         List assumptions of material properties here...
 
-        :param compressive_strength: Concrete compressive strength
-        :param colour: Colour of the concrete for rendering
+        Args:
+            compressive_strength: Concrete compressive strength
+            colour: Colour of the concrete for rendering
 
-        :return: Concrete material object
+        Returns:
+            Concrete material object
         """
-
         raise NotImplementedError
 
     def create_steel_material(
@@ -56,12 +57,13 @@ class DesignCode:
 
         List assumptions of material properties here...
 
-        :param yield_strength: Steel yield strength
-        :param colour: Colour of the steel for rendering
+        Args:
+            yield_strength: Steel yield strength
+            colour: Colour of the steel for rendering
 
-        :return: Steel material object
+        Returns:
+            Steel material object
         """
-
         raise NotImplementedError
 
     def get_gross_properties(
@@ -70,12 +72,13 @@ class DesignCode:
     ) -> res.GrossProperties:
         """Returns the gross section properties of the reinforced concrete section.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.get_gross_properties`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.get_gross_properties`
 
-        :return: Concrete properties object
+        Returns:
+            Concrete properties object
         """
-
         return self.concrete_section.get_gross_properties(**kwargs)
 
     def get_transformed_gross_properties(
@@ -84,12 +87,13 @@ class DesignCode:
     ) -> res.TransformedGrossProperties:
         """Transforms gross section properties.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.get_transformed_gross_properties`
+        Args"
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.get_transformed_gross_properties`
 
-        :return: Transformed concrete properties object
+        Returns:
+            Transformed concrete properties object
         """
-
         return self.concrete_section.get_transformed_gross_properties(**kwargs)
 
     def calculate_cracked_properties(
@@ -98,27 +102,28 @@ class DesignCode:
     ) -> res.CrackedResults:
         """Calculates cracked section properties.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_cracked_properties`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_cracked_properties`
 
-        :return: Cracked results object
+        Returns:
+            Cracked results object
         """
-
         return self.concrete_section.calculate_cracked_properties(**kwargs)
 
     def moment_curvature_analysis(
         self,
         **kwargs,
     ) -> res.MomentCurvatureResults:
-        """Performs a moment curvature analysis. No reduction factors are applied to the
-        moments.
+        """Performs a moment curvature analysis (no reduction factors applied).
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.moment_curvature_analysis`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.moment_curvature_analysis`
 
-        :return: Moment curvature results object
+        Returns:
+            Moment curvature results object
         """
-
         return self.concrete_section.moment_curvature_analysis(**kwargs)
 
     def ultimate_bending_capacity(
@@ -127,12 +132,13 @@ class DesignCode:
     ) -> res.UltimateBendingResults:
         """Calculates the ultimate bending capacity.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.ultimate_bending_capacity`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.ultimate_bending_capacity`
 
-        :return: Ultimate bending results object
+        :Returns:
+            Ultimate bending results object
         """
-
         return self.concrete_section.ultimate_bending_capacity(**kwargs)
 
     def moment_interaction_diagram(
@@ -141,12 +147,13 @@ class DesignCode:
     ) -> res.MomentInteractionResults:
         """Generates a moment interaction diagram.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.moment_interaction_diagram`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.moment_interaction_diagram`
 
-        :return: Moment interaction results object
+        Returns:
+            Moment interaction results object
         """
-
         return self.concrete_section.moment_interaction_diagram(**kwargs)
 
     def biaxial_bending_diagram(
@@ -155,42 +162,43 @@ class DesignCode:
     ) -> res.BiaxialBendingResults:
         """Generates a biaxial bending diagram.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.biaxial_bending_diagram`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.biaxial_bending_diagram`
 
-        :return: Biaxial bending results
+        Returns:
+            Biaxial bending results
         """
-
         return self.concrete_section.biaxial_bending_diagram(**kwargs)
 
     def calculate_uncracked_stress(
         self,
         **kwargs,
     ) -> res.StressResult:
-        """Calculates stresses within the reinforced concrete section assuming an
-        uncracked section.
+        """Calculates uncracked stresses within the reinforced concrete section.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_uncracked_stress`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_uncracked_stress`
 
-        :return: Stress results object
+        Returns:
+            Stress results object
         """
-
         return self.concrete_section.calculate_uncracked_stress(**kwargs)
 
     def calculate_cracked_stress(
         self,
         **kwargs,
     ) -> res.StressResult:
-        """Calculates stresses within the reinforced concrete section assuming a cracked
-        section.
+        """Calculates cracked stresses within the reinforced concrete section.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_cracked_stress`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_cracked_stress`
 
-        :return: Stress results object
+        Returns:
+            Stress results object
         """
-
         return self.concrete_section.calculate_cracked_stress(**kwargs)
 
     def calculate_service_stress(
@@ -199,12 +207,13 @@ class DesignCode:
     ) -> res.StressResult:
         """Calculates service stresses within the reinforced concrete section.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_service_stress`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_service_stress`
 
-        :return: Stress results object
+        Returns:
+            Stress results object
         """
-
         return self.concrete_section.calculate_service_stress(**kwargs)
 
     def calculate_ultimate_stress(
@@ -213,10 +222,11 @@ class DesignCode:
     ) -> res.StressResult:
         """Calculates ultimate stresses within the reinforced concrete section.
 
-        :param kwargs: Keyword arguments passed to
-            :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_ultimate_stress`
+        Args:
+            kwargs: Keyword arguments passed to
+                :meth:`~concreteproperties.concrete_section.ConcreteSection.calculate_ultimate_stress`
 
-        :return: Stress results object
+        Returns:
+            Stress results object
         """
-
         return self.concrete_section.calculate_ultimate_stress(**kwargs)
